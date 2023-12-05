2a648293e83f83bc60e46ccd378c4f9a6f878345
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2023-12-05 12:35:02 +0530

    Add files via upload

---
+++ FrontEnd/src/app/registeremployee/registeremployee.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegisteremployeeComponent } from './registeremployee.component';
+
+describe('RegisteremployeeComponent', () => {
+  let component: RegisteremployeeComponent;
+  let fixture: ComponentFixture<RegisteremployeeComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RegisteremployeeComponent]
+    });
+    fixture = TestBed.createComponent(RegisteremployeeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
